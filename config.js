2a79ded62758a9b9674e420269e7b07f32e5ff5c
alexanderfsm <94754185+alexanderfsm@users.noreply.github.com>
2022-10-23 16:20:56 -0300

    Updated

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// for token 32 caracters https://generate-secret.vercel.app/32
-// for token 64 caracters https://generate-secret.vercel.app/64
-// please change this token by an example above
-export const token =  "9208f87c0c0886c5c1c569bbf795bb3d";
+export const token = "e632efe_token_here_5452b";
 export const CRUDHost = "https://examplename.vercel.app";
-export const CRUDToken = "Bearer 56fc51b1abf1a6fa37fe4d9c2d9e016c92a4a19afc848ca3c4dc4d00466dac7f";
+export const CRUDToken = "e632efe_token_here_5452b";
+//optional
+export const telegramBotToken = "";
+export const telegramIDGroup = "";
--- pages/api/new.js
+++ pages/api/new.js
@@ -1,32 +1,50 @@
-import { CRUDHost, CRUDToken } from '../../config';
+import { CRUDHost, CRUDToken, telegramBotToken, telegramIDGroup } from '../../config';
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const axios = require('axios');
-  const type = req.body.type;
-  const text = req.body.text;
-  if(['bug', 'upgrade', 'report'].includes(type) && text.length > 0 && text.length < 250){
-    axios({
-      method: 'post',
-      url: CRUDHost+'/api/create',
-      headers: {'authorization': CRUDToken},
-      data: {
+  if ([1, 2, 3, 4, 5].includes(req.body.level) && req.body.text.length > 0 && req.body.text.length < 250) {
+    fetch(CRUDHost + '/api/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': CRUDToken
+      },
+      body: JSON.stringify({
         table: {
-          name: "feedback"
+          name: "FeedbackForWeb"
         },
         column: {
-          type: type,
-          text: text
-        },
-      }
+          level: req.body.level,
+          text: req.body.text,
+          source: req.body.source,
+          version: req.body.version,
+          uniqueId: req.body.uniqueId,
+          systemName: req.body.systemName,
+          systemVersion: req.body.systemVersion,
+          timestamp: new Date().getTime()
+        }
+      })
     })
     .then(function (response) {
+      let message;
+      if (req.body.source) {
+        message = "Um novo FeedbackForWeb foi registrado %0aSource: " + req.body.source + " %0aID: " + response.data + "%0aLevel: " + req.body.level;
+      } else {
+        message = "Um novo FeedbackForWeb foi registrado %0aID: " + response.data + "%0aLevel: " + req.body.level;
+      }
+      fetch('https://api.telegram.org/bot' + telegramBotToken + '/sendMessage?chat_id=' + telegramIDGroup + '&text=' + message).catch(function (error) {
+        res.status(500).send(error);
+      });
       res.status(200).send(response.data);
     })
     .catch(function (error) {
-      res.status(500).send(error);
+      if (error.response.status == 400) {
+        res.status(400).send(error.response.data);
+      } else {
+        res.status(500).send(error);
+      }
     });
-  }else{
-    res.status(204).send("no data send.");
-  }  
+  } else {
+    res.status(400).send('Information send is incorrect.');
+  }
 }
