a1579e7a7a090e9131c57f18fc64c0ba1a3a1dba
Alexander <94754185+alexanderfsm@users.noreply.github.com>
2022-11-02 17:39:32 -0300

    Updated

--- pages/api/new.js
+++ pages/api/new.js
@@ -43,13 +43,13 @@ export default function handler(req, res) {
           })
         })
         .then(function () {
-          res.status(200).json('Feedback registred.');
+          res.status(200).json(data);
         })
         .catch(function (error) {
           res.status(500).json(error);
         });
       } else {
-        res.status(200).json('Feedback registred.');
+        res.status(200).json(data);
       }
     })
     .catch(function (error) {
